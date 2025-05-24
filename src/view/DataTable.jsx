ce64fc0dfbde258ae7b175c582b46c4b87aeb764
Albert Oliva <oliva.albert+1@gmail.com>
2025-05-25 00:11:12 +0200

    add more pagination

--- src/view/DataTable.jsx
+++ src/view/DataTable.jsx
@@ -230,6 +230,9 @@ export const DataTable = () => {
                     <option value="10">10</option>
                     <option value="20">20</option>
                     <option value="30">30</option>
+                    <option value="40">40</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
                 </select>
             </div>
         </div>
