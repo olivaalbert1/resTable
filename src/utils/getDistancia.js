edb6b99e9f24393f916e811c4d6d0ebdb7ece77a
Albert Oliva <oliva.albert+1@gmail.com>
2025-05-25 00:11:34 +0200

    be defensive against empty maps url

--- src/utils/getDistancia.js
+++ src/utils/getDistancia.js
@@ -1,9 +1,14 @@
 // Objetivo: Calcular a distância entre dois pontos
 export const getDistancia = (url) => {
-    let distancia = {
-        latitud: url.split('!3d')[1].split('!4d')[0],
-        longitud: url.split('!4d')[1].split('!')[0]
+    let distancia
+    // Si la variable url no contiene 'maps.google.com', retornar un objeto con latitud y longitud como 0
+    if (!url.includes('!3d') || !url.includes('!4d')) {
+        return distancia = { latitud: 0, longitud: 0 };
+    } else {
+        // Si la url no contiene los parámetros necesarios, retornar un objeto con latitud y longitud como 0
+        return distancia = {
+            latitud: url.split('!3d')[1].split('!4d')[0],
+            longitud: url.split('!4d')[1].split('!')[0]
+        };
     }
-
-    return distancia
 }
