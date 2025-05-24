df41afebb6abcc651d99dd88583cf5834bb9e203
Albert Oliva <oliva.albert+1@gmail.com>
2025-05-25 00:31:25 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "ES2022" // Set the target to ES2022 for better compatibility with modern browsers
+  },
 })
